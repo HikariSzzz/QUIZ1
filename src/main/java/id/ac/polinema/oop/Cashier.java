d4664020f62c90870a1a8c65d249d50564a8d199
HikariSzzz <varellnashwaalifiowijaya@gmail.com>
2026-09-25 10:44:35 +0700

    cm4

--- src/main/java/id/ac/polinema/oop/Cashier.java
+++ src/main/java/id/ac/polinema/oop/Cashier.java
@@ -1,6 +1,14 @@
 package id.ac.polinema.oop;
 
 public class Cashier {
-    
+    private String cashierName;
+
+    public Cashier(String cashierName) {
+        this.cashierName = cashierName;
+    }
+
+    public String getCashierName() {
+        return cashierName;
+    }
 }
 
