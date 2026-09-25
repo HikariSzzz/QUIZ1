9bc580f58455725b43eef51b9616044187dd7bfd
HikariSzzz <varellnashwaalifiowijaya@gmail.com>
2026-09-25 10:55:39 +0700

    teskasir

--- src/main/java/id/ac/polinema/oop/Cashier.java
+++ src/main/java/id/ac/polinema/oop/Cashier.java
@@ -10,12 +10,5 @@ public class Cashier {
     public String getCashierName() {
         return cashierName;
     }
-
-    public double processPayment(Order order, double amountPaid) {
-        double total = order.getTotalPrice();
-        if (amountPaid < total) {
-            return -1;
-        }
-    }
 }
 
